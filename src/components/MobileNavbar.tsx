0186f0fcd94387ec0e7b1dabb8e9dfb2dd0d45ee
ponkenman <gg7958209@gmail.com>
2025-05-24 13:03:33 +1000

    Redo tier list styling

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function MobileNavbar({ color }: { color: string}) {
     const [mobileNavVisible, setMobileNavVisible] = useState(false);
@@ -18,26 +18,30 @@ export function MobileNavbar({ color }: { color: string}) {
 
     // Prevent scrolling if visible
     useEffect(() => {
+        let timer: ReturnType<typeof setTimeout>;
         if (mobileNavVisible) {
             document.body.style.top = `-${window.scrollY}px`;
             document.body.style.position = "fixed";
         } else {
-            const top = document.body.style.top;
+            timer = setTimeout(() => {
+                            const top = document.body.style.top;
             const topString = top.substring(top[0] === '0' ? 0 : 1, top.length - 2);
             const scrollY = parseInt(topString) - window.innerHeight;
             if (!isNaN(scrollY)) {
-                document.body.style.position = '';
+                document.body.style.position = 'unset';
                 document.body.style.top = '';
                 window.scrollTo(0, parseInt(`${scrollY}` || '0'));
             }
+            }, 500);
         }
+        return () => clearTimeout(timer);
     }, [mobileNavVisible]);
 
     return (<>
     { mobileNavVisible
     ? <>
         <nav className="flex h-screen z-1">
-            <div className={`${color} fixed md:hidden m-auto z-1 inset-0 overscroll-none flex flex-col items-center justify-center gap-5`}>
+            <div style={{animation: "0.5s ease-out fadeInUpNav"}} className={`${color} fadeIn fixed md:hidden m-auto z-1 inset-0 overscroll-none flex flex-col items-center justify-center gap-5`}>
                 <a style={{animation: "0.5s ease-out fadeInUpNav"}} className="cursor-pointer font-bold hover:underline text-xl">HOME</a>
                 <a style={{animation: "0.5s ease-out fadeInUpNav"}} className="cursor-pointer font-bold hover:underline text-xl">FUCK</a>
                 <a style={{animation: "0.5s ease-out fadeInUpNav"}} className="cursor-pointer font-bold hover:underline text-xl">THESE</a>
